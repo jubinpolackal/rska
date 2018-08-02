2f3f2394e2b606c28ce89dd1c22deb54f1d1a583
Jubin Jose <jubinpolackal@gmail.com>
2018-08-01 23:49:21 -0300

    Initial setup

---
+++ Projects/rska/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var http = require('http');
+var https = require('https');
+var path = require('path');
+var fs = require('fs');
+var jwt = require('jsonwebtoken');
+var config = require('./config');
+
+const app = express();
+
+// Parsers
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist/rska')));
+
+
+//Unprotected routes
+app.get('/', (req, res)=>{
+  console.log("Sending index.html ...");
+  res.sendFile(path.join(__dirname, 'dist/rska'));
+});
+
+const server = http.createServer(app);
+//Set Port
+const port = process.env.PORT || config.server.port;
+server.listen(port, () => {
+  console.log('RSKA started on '+config.server.host+':'+config.server.port);
+});
---
+++ Projects/rska/config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+
+config.server = {
+  host:'localhost',
+  port:'3000'
+};
+
+//Secret key for json web token
+config.jwtSecretKey = 'yG5SZyflp4';
+
+module.exports = config;
