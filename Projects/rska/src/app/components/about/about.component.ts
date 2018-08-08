b663949a48b87509aa1171d28c372a1d862727ff
Jubin Jose <jubinpolackal@gmail.com>
2018-08-07 22:03:10 -0300

    Added the front page slider

--- Projects/rska/src/app/components/about/about.component.ts
+++ Projects/rska/src/app/components/about/about.component.ts
@@ -6,8 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  // The time to show the next photo
+  nextSlideInterval = 3000;
+  // Looping or not
+  private noLoopSlides = false;
+  // Photos
+  slides: Array<any> = [];
 
-  constructor() { }
+  constructor() {
+    this.slides.push(
+      {image: '../../../assets/images/about/1.jpg', text: 'Caption #1'},
+      {image: '../../../assets/images/about/2.jpg', text: 'Caption #2'},
+      {image: '../../../assets/images/about/3.jpg', text: 'Caption #3'}
+  );
+  }
 
   ngOnInit() {
   }
